2f038a1b0f2fbfceef4046dafdab3b836fc7ee7e
Joener Münch <joener.munch@gmail.com>
2020-01-17 14:58:58 -0300

    added footer

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const projectsButton = document.querySelector(".projects-button");
 const arrow = document.querySelector(".arrow");
 const contactArea = document.querySelector(".contact");
 const contactButton = document.querySelector(".contact-button");
+const emailButton = document.querySelector(".email-button");
+const emailSpan = document.querySelector(".email-span");
+
+emailButton.addEventListener("click", () => {
+  emailSpan.style.display === "inline"
+    ? (emailSpan.style.display = "none")
+    : (emailSpan.style.display = "inline");
+});
 
 const smoothScroll = section => {
   section.scrollIntoView({ block: "start", behavior: "smooth" });
