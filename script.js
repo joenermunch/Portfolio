a0eeb4757bc57615c332b0d584cc940d8ceb80ec
Joener Münch <joener.munch@gmail.com>
2020-01-20 14:43:53 -0300

    added assets and fixed styling bugs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const projectsButton = document.querySelector(".projects-button");
 const arrow = document.querySelector(".arrow");
 const contactArea = document.querySelector(".contact");
 const contactButton = document.querySelector(".contact-button");
-const emailButton = document.querySelector(".email-button");
+const emailButton = document.querySelector(".email");
 const emailSpan = document.querySelector(".email-span");
 
 emailButton.addEventListener("click", () => {
